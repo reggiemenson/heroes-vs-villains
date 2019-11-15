bd068ecb9806c7dbae2afea18a634003922ead60
kwaku <44327202+mr-nansi@users.noreply.github.com>
2019-11-15 22:56:42 +0000

    added chance game

--- src/app.js
+++ src/app.js
@@ -1,16 +1,78 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Calculator from './components/Calculator'
+
 class App extends React.Component {
 
-  constructor() {
-    super()
-    this.state = {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      playerChoice: 'Player choice',
+      computerChoice: 'Computer choice',
+      result: 'Choose your weapon!'
+    }
+    this.iChooseYou = this.iChooseYou.bind(this)
+    this.doOver = this.doOver.bind(this)
+  }
+
+  iChooseYou(choice) {
+    let result = ''
+    this.setState({
+      playerChoice: choice
+    })
+    console.log(choice)
+    setTimeout(function () {
+      this.whatYouGot()
+      result = this.whoWins()
+      console.log(this.whoWins())
+      this.setState({
+        result
+      })
+    }.bind(this), 2000)
+  }
+
+  whatYouGot() {
+    const choices = ['rock', 'paper', 'scissors']
+    const throwDown = choices[Math.round(Math.random() * 2)]
+    this.setState({
+      computerChoice: throwDown
+    })
+  }
+
+  whoWins() {
+    if (this.state.playerChoice !== this.state.computerChoice) {
+      switch (this.state.playerChoice) {
+        case 'rock':
+          return this.state.computerChoice === 'paper' ? 'You lose!' : 'You win!'
+        case 'scissors':
+          return this.state.computerChoice === 'rock' ? 'You lose!' : 'You win!'
+        case 'paper':
+          return this.state.computerChoice === 'scissors' ? 'You lose!' : 'You win!'
+      }
+    } else return 'It\'s a draw!'
+  }
+
+  doOver(){
+    this.setState({
+      playerChoice: 'Player choice',
+      computerChoice: 'Computer choice',
+      result: 'Choose your weapon!'
+    })
   }
 
   render() {
     return (
-      <h1>Hello world</h1>
+      <div>
+        <h1>Hello world</h1>
+        <Calculator 
+          pChoice={this.state.playerChoice}
+          cChoice={this.state.computerChoice}
+          result={this.state.result}
+          iChooseYou={this.iChooseYou}
+          doOver={this.doOver}
+        />
+      </div>
     )
   }
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+class Calculator extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>Player 1 chose: <span>{this.props.pChoice}</span></p>
+        <p>Player 2 chose: <span>{this.props.cChoice}</span></p>
+        <p className={'result'}>{this.props.result}</p>
+        <button onClick={(e) => this.props.iChooseYou(e.target.innerHTML)}>rock</button>
+        <button onClick={(e) => this.props.iChooseYou(e.target.innerHTML)}>paper</button>
+        <button onClick={(e) => this.props.iChooseYou(e.target.innerHTML)}>scissors</button>
+        <button onClick={() => this.props.doOver()}>reset</button>
+      </div>
+    )
+  }
+}
+
+export default Calculator
